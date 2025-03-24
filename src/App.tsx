8143e0966875ab253a9f62e121b5b55180f60a94
Ankit <brijtest09@gmail.com>
2025-03-24 08:34:17 +0530

    FINISHED WITH ROUTING

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import './App.css'
+import {Route,Routes} from 'react-router-dom'
+import Blogs from './pages/Blogs'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import Blog from './pages/Blog'
 
 function App() {
 
   return (
-    <>
-      <div>  
-        App
-      </div>
-    </>
+    <Routes>
+      <Route path='/' element={<Blogs/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/signin' element={<Signin/>}/>
+      <Route path='/blog/:id' element={<Blog />}/>
+    </Routes>
   )
 }
 
